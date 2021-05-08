fe1948fbeb58ed3703e3e0671857038a369d1352
Cloud9-ine <68072743+Cloud9-ine@users.noreply.github.com>
2021-05-07 20:03:44 -0400

    Project: Multi-scale Hierarchical (Cell Segment)

---
+++ DeepLearning_Spring2021_Final_Project/rename.py
@@ -0,0 +1,54 @@
+import os
+
+########################################################################################################################
+# Stage1_test
+########################################################################################################################
+# folder_path = "data-science-bowl-2018/stage1_test"
+# fileList = os.listdir(folder_path)
+# os.chdir(folder_path)
+# cnt = 0
+# for oldName in fileList:
+#     newName = str(cnt)
+#     os.rename(oldName, newName)
+#     cnt += 1
+
+# for i in range(65):
+#     folder_path = "D:/SunXuhua/University/JHU_SP21_Baltimore/EN601.682 Deep Learning/project/DL_project/"
+#     folder_path = folder_path + "data-science-bowl-2018/stage1_test/" + str(i) +"/images"
+#     fileList = os.listdir(folder_path)
+#     os.chdir(folder_path)
+#     for oldName in fileList:
+#         newName = str(i) + ".png"
+#         os.rename(oldName, newName)
+
+########################################################################################################################
+# Stage1_train
+########################################################################################################################
+# folder_path = "data-science-bowl-2018/stage1_train"
+# fileList = os.listdir(folder_path)
+# os.chdir(folder_path)
+# cnt = 0
+# for oldName in fileList:
+#     newName = str(cnt)
+#     os.rename(oldName, newName)
+#     cnt += 1
+
+# for i in range(670):
+#     folder_path = "D:/SunXuhua/University/JHU_SP21_Baltimore/EN601.682 Deep Learning/project/DL_project/"
+#     folder_path = folder_path + "data-science-bowl-2018/stage1_train/" + str(i) +"/images"
+#     fileList = os.listdir(folder_path)
+#     os.chdir(folder_path)
+#     for oldName in fileList:
+#         newName = str(i) + ".png"
+#         os.rename(oldName, newName)
+
+# for i in range(670):
+#     folder_path = "D:/SunXuhua/University/JHU_SP21_Baltimore/EN601.682 Deep Learning/project/DL_project/"
+#     folder_path = folder_path + "data-science-bowl-2018/stage1_train/" + str(i) +"/masks"
+#     fileList = os.listdir(folder_path)
+#     os.chdir(folder_path)
+#     cnt = 0
+#     for oldName in fileList:
+#         newName = str(i) + "_" + str(cnt) + ".png"
+#         os.rename(oldName, newName)
+#         cnt += 1
